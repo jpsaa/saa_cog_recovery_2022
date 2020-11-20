663342120d0ff1356fd3638152afe7add652c379
Juan Pablo Saa <44592106+jpsaa@users.noreply.github.com>
2020-11-20 16:10:16 +1100

    Update create_table_4.R

--- R/create_table_4.R
+++ R/create_table_4.R
@@ -1,8 +1,8 @@
-### Table 4
-create_table_4 <- function (data,
+### Table e-2
+create_table_e_2 <- function (data,
                             columns) {
   
-  t4 <- data %>% 
+  te2 <- data %>% 
     select(columns) %>% 
     group_by(trends) %>%
     summarise_if(is.numeric, 
@@ -11,67 +11,67 @@ create_table_4 <- function (data,
                       ~ IQR(., na.rm = TRUE)))
   
   ### getting descriptive stats for all variables
-  counts <- names(t4)[grep("sum", names(t4))]
-  medians <- names(t4)[grep("median", names(t4))]
-  iqrs <- names(t4)[grep("IQR", names(t4))]
-  table4 <- matrix(nrow = nrow(t4))
+  counts <- names(te2)[grep("sum", names(te2))]
+  medians <- names(te2)[grep("median", names(te2))]
+  iqrs <- names(te2)[grep("IQR", names(te2))]
+  table_e2 <- matrix(nrow = nrow(te2))
   
   for (i in seq_along(medians)) {
     
     #### looking for NA values and turning them into spaces
-    .counts <- which(names(t4) == counts[i])
-    .med <- which(names(t4) == medians[i])
-    .iqr <- which(names(t4) == iqrs[i])
+    .counts <- which(names(te2) == counts[i])
+    .med <- which(names(te2) == medians[i])
+    .iqr <- which(names(te2) == iqrs[i])
     
-    table4 <- cbind(
-      table4,
+    table_e2 <- cbind(
+      table_e2,
       cbind(
-        paste0(round(as.numeric(unlist(t4[, .counts])), 2), ", ",
-               round(as.numeric(unlist(t4[, .med])), 2), " (",
-               paste0(round(as.numeric(unlist(t4[, .iqr])), 2), ")"))))
-    table4[grep("NA", table4[, i + 1]), i + 1] = ""
+        paste0(round(as.numeric(unlist(te2[, .counts])), 2), ", ",
+               round(as.numeric(unlist(te2[, .med])), 2), " (",
+               paste0(round(as.numeric(unlist(te2[, .iqr])), 2), ")"))))
+    table_e2[grep("NA", table_e2[, i + 1]), i + 1] = ""
     
   }
   
-  table4 <- table4[, -1]
-  table4 <- t(table4)
+  table_e2 <- table_e2[, -1]
+  table_e2 <- t(table_e2)
   
-  table4 <- rbind(
+  table_e2 <- rbind(
     get_stats(data, id),
     get_stats(data, gender_w1, "male"),
     get_stats(data, educ_binary, "secondary or more"),
     get_stats(data, marital_status_binary_w1, "married"),
     get_stats(data, disab_prestroke, "some disab"),
-    table4)
-  colnames(table4) <- unlist(t4[, 1])
+    table_e2)
+  colnames(table_e2) <- unlist(te2[, 1])
   
   ####rownames
-  table4 <- cbind(Variable = c(
+  table_e2 <- cbind(Variable = c(
     "n (%)", "Males (%)", "Education",
     "Marital status", "mrs prestroke",
     trimws(gsub("median|t0|.y|_", " ", medians))
-  ), table4)
+  ), table_e2)
   
   ### creating space between assessments
   cols <- c("stolic bp", 'moca', 'mmse', 'stroop',
             "ravens", 'time taken', 'lds', 'madrs',
             'nihss', 'barthel', 'mrs score', 
             'aerobic', 'acs', 'wsas', 'sis')
-  .t4 <- matrix(ncol = ncol(table4))
+  .te2 <- matrix(ncol = ncol(table_e2))
   
   grab <- 1
   
   for (i in seq_along(cols)) {
     
-    space <- first(grep(cols[i], table4[, 1])) - 1
-    .t4 <- rbind(.t4, table4[grab:space, ], "")
-    grab <- first(grep(cols[i], table4[, 1]))
+    space <- first(grep(cols[i], table_e2[, 1])) - 1
+    .te2 <- rbind(.te2, table_e2[grab:space, ], "")
+    grab <- first(grep(cols[i], table_e2[, 1]))
     
   }
   
   #### adding the last category
-  .t4 <- rbind(.t4[-1, ], 
-               table4[grep("sis", table4[, 1]), ])
+  .te2 <- rbind(.te2[-1, ], 
+               table_e2[grep("sis", table_e2[, 1]), ])
 
 }
 
